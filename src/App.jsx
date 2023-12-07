fc872f35081eff379e2b9e752d74e50909a040f7
mehrnaz <pishyar.mehrnaz@gmail.com>
2023-12-07 13:36:32 +0100

    add locationList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-
+import { Toaster } from "react-hot-toast";
 import "./App.css";
 import Header from "./components/Header/Header";
+import LocationList from "./components/LocationList/LocationList";
 
 function App() {
   return (
     <div>
-    <Header/>
+      <Toaster />
+      <Header />
+      <LocationList />
     </div>
-  )
+  );
 }
 
 export default App;
-
