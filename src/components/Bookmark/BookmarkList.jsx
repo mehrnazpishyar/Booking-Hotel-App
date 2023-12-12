8ce77364e97eb27b61d355585f556bd4970185d4
mehrnaz <pishyar.mehrnaz@gmail.com>
2023-12-12 09:39:44 +0100

    delete bookmark

--- src/components/Bookmark/BookmarkList.jsx
+++ src/components/Bookmark/BookmarkList.jsx
@@ -1,12 +1,21 @@
 import ReactCountryFlag from "react-country-flag";
-// import Loader from "../Loader/Loader";
+import Loader from "../Loader/Loader";
 import { useBookmark } from "../context/BookmarkListContext";
 import { Link } from "react-router-dom";
+import { HiTrash } from "react-icons/hi";
 
 const BookmarkList = () => {
-  const {  bookmarks,currentBookmark } = useBookmark();
+  const { isLoading, bookmarks, currentBookmark, deleteBookmark } =
+    useBookmark();
 
-  // if (isLoading) return <Loader />;
+  const handleDelete = async (e, id) => {
+    e.preventDefault();
+    await deleteBookmark(id);
+  };
+
+  if (isLoading) return <Loader />;
+  if (!bookmarks.length) return <p>there is no bookmark location</p>;
+
   return (
     <div>
       <h2>BookmarkList</h2>
@@ -18,9 +27,18 @@ const BookmarkList = () => {
               key={item.id}
               to={`${item.id}?lat=${item.latitude}&lng=${item.longitude}`}
             >
-              <div className={`bookmarkItem ${item.id === currentBookmark?.id ? "current-bookmark" :""}`}>
-                <ReactCountryFlag svg countryCode={item.countryCode} />
-                &nbsp; <strong>{item.cityName}</strong> &nbsp;
+              <div
+                className={`bookmarkItem ${
+                  item.id === currentBookmark?.id ? "current-bookmark" : ""
+                }`}
+              >
+                <div>
+                  <ReactCountryFlag svg countryCode={item.countryCode} />
+                  &nbsp; <strong>{item.cityName}</strong> &nbsp;
+                </div>
+                <button onClick={(e) => handleDelete(e, item.id)}>
+                  <HiTrash className="trash" />
+                </button>
               </div>
             </Link>
           );
